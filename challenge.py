b4edfa34e253cc7fb8d8254658edcb2ff6bb4365
blarzhgit <muhammadbadrululum@gmail.com>
2026-01-14 10:35:20 +0700

    first commit

---
+++ challenge.py
@@ -0,0 +1,8 @@
+HP = 100
+potion = 25
+monster = 40
+
+HP = HP + potion
+print(HP)
+HP = HP - monster
+print(HP)
---
+++ chatbot.py
@@ -0,0 +1,7 @@
+print("Halo! Saya adalah chatbot sederhana. Mari kita saling mengenal")
+nama = input("Namamu siapa ? ")
+umur = input("Usiamu berapa ? ")
+makanan = input("Makanan favoritmu apa? ")
+
+kesimpulan = f"Terima kasih, jadi namamu adalah {nama}, kamu berumur {umur} tahun, dan makanan favoritmu adalah {makanan}. Senang mengenalmu !!"
+print(kesimpulan)
